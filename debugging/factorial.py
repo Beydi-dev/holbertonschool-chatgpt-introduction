8b1aa4db72bd5e4a5da10369519f08e566cd5f0f
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-05-05 09:51:53 +0200

    First Try

---
+++ debugging/factorial.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if len(sys.argv) != 2:
+    print("Usage: ./factorial.py <non-negative integer>")
+    sys.exit(1)
+
+try:
+    num = int(sys.argv[1])
+    if num < 0:
+        raise ValueError("Negative value not allowed")
+except ValueError as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+
+f = factorial(num)
+print(f)
